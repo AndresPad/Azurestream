e331851d994925c71b2cbe8c81f98bb9bb5e7d73
Andres Padilla <69865428+AndresPad@users.noreply.github.com>
2022-02-24 21:36:01 -0500

    python

---
+++ azurestream.python/azurestream.python.poc.2.py
@@ -0,0 +1,7 @@
+import os, uuid, sys
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+msg = "Hello World"
+print(msg)
